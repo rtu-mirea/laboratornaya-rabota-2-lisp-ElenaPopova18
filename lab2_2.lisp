d5cb059484a0c2c4619ca6ebc47fecf7b6f1dd47
ElenaPopova18 <56185573+ElenaPopova18@users.noreply.github.com>
2020-01-16 19:12:43 +0300

    Добавление в задание 2 входного параметра пути

--- lab2_2.lisp
+++ lab2_2.lisp
@@ -1,7 +1,7 @@
 (defconstant myfile "C:/Users/User/Downloads/roswell/myfile.txt")
 
 (defun myread (filepath)
-(let ((in (open filePath :if-does-not-exist nil)))
+(let ((in (open filepath :if-does-not-exist nil)))
   (when in
     (loop for line = (read-line in nil)
          while line do (format t "~a~%" line))
